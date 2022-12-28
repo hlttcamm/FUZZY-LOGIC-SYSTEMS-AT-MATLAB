3950966c6b0c2705dc20075c760c380956ac637b
Halit <hltcamm@gmail.com>
2022-12-28 23:32:36 +0300

    fibonacci added

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+cube = lambda x: x**3
+
+def fibonacci(n):
+    agg=0
+    agg=1
+    list=[]
+    for _ in range(n):
+        agg1=i
+
+        list.append(agg)
+        print(list)
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
